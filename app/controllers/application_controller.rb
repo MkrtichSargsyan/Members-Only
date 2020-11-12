036063f36d547705f41dde197d16f668fa4c51b7
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-12 14:28:34 +0400

    add posts controller and index.html.erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   
+  def current_user
+    return unless session[:user_id]
+
+    @current_user ||= User.find(session[:user_id])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,12 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: %i[new create]
 
   def index
+
     @posts = Post.all
   end
 
-  def new
-    
-  end
+  def new; end
 
-  def create
-    
-  end
-
-  
+  def create; end
 end
