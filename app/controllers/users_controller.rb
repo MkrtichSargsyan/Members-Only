96226f833187e693847e63a2a891f89050e48a81
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-16 03:03:07 +0400

    extend device for adding username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
