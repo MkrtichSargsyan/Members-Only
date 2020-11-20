466003d66d951c3e3554dc5dbb0463d035354053
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-20 07:02:08 +0400

    separate logic from _post.html.erb file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,28 @@ module ApplicationHelper
     </li>".html_safe
     end
   end
+
+  def show_image(user)
+    if signed_in?
+      "<div class='image_container'>
+        #{avatar_attached?(user)}
+      </div>
+      <h3 class='card_username'>#{user.name}</h3>
+      ".html_safe
+    else
+      "<div class='image_container'>
+        #{image_tag('avatar.jpg', alt: 'avatar', class: 'avatar_image')}
+      </div>
+      <h3 class='card_username'>Anonymous</h3>
+    ".html_safe
+    end
+  end
+
+  def avatar_attached?(user)
+    if user.avatar.attached?
+      image_tag(user.avatar, alt: 'avatar', class: 'avatar_image')
+    else
+      image_tag('avatar.jpg', alt: 'avatar', class: 'avatar_image')
+    end
+  end
 end
