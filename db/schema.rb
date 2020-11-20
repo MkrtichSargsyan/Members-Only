a9a71524e75c53856d133e34e3f0e393593447fb
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-20 10:37:23 +0400

    change ids

--- db/schema.rb
+++ db/schema.rb
@@ -10,51 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_118_011_750) do
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.integer 'record_id', null: false
-    t.integer 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+ActiveRecord::Schema.define(version: 2020_11_18_011750) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'name'
-    t.string 'status'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'posts', 'users'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 User.create!([
-  {email: "tyler@gmail.com", encrypted_password: "$2a$12$Hkjbgbjzh6kMe3/0444LOOeZav66xUH6shhwxjRmS2y8pgEwTGSWG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "Tyler Durden", status: nil},
-  {email: "marla@gmail.com", encrypted_password: "$2a$12$gIBeeXu6Hy3lVSLuncFMNur.0oeyXG/s93QyRjaBZnO7o40UF4o1.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "Marla Singer", status: nil},
-  {email: "mkrtichsargsyan24@gmail.com", encrypted_password: "$2a$12$JhCfOWzOQ/3iBoniKIn14uHZ5/5jlC52S0GmiGaCRPQM2aH3K0mTe", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "Mike Sargsyan", status: nil}
+  {email: "tyler@gmail.com",password:'aaaaaa', encrypted_password: "$2a$12$Hkjbgbjzh6kMe3/0444LOOeZav66xUH6shhwxjRmS2y8pgEwTGSWG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "Tyler Durden", status: nil},
+  {email: "marla@gmail.com",password:'aaaaaa', encrypted_password: "$2a$12$gIBeeXu6Hy3lVSLuncFMNur.0oeyXG/s93QyRjaBZnO7o40UF4o1.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "Marla Singer", status: nil},
+  {email: "mkrtichsargsyan24@gmail.com",password:'aaaaaa', encrypted_password: "$2a$12$JhCfOWzOQ/3iBoniKIn14uHZ5/5jlC52S0GmiGaCRPQM2aH3K0mTe", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, name: "Mike Sargsyan", status: nil}
 ])
 Post.create!([
-  {user_id: 27, title: "Delete your social media", description: "Quitting social media is the most finely targeted way to resist the insanity of our times."},
-  {user_id: 27, title: "Vandalizing", description: "Go and destroy a piece of corporate art"},
-  {user_id: 28, title: "Fight with stranger", description: "Get into a fight , lose, then show up to work or school with those bruises and act like nothing happened."},
-  {user_id: 31, title: "Barcelona jersey", description: "Wear a Barcelona jersey in Madrid"}
+  {user_id: 1, title: "Delete your social media", description: "Quitting social media is the most finely targeted way to resist the insanity of our times."},
+  {user_id: 1, title: "Vandalizing", description: "Go and destroy a piece of corporate art"},
+  {user_id: 2, title: "Fight with stranger", description: "Get into a fight , lose, then show up to work or school with those bruises and act like nothing happened."},
+  {user_id: 3, title: "Barcelona jersey", description: "Wear a Barcelona jersey in Madrid"}
 ])
 ActiveStorage::Blob.create!([
   {key: "i82b83pao52fkqoady2hskldtr1p", filename: "dd.png", content_type: "image/png", metadata: {"identified"=>true, "analyzed"=>true}, byte_size: 192557, checksum: "4/g4N1GsCXUqCZpaalNEBQ=="},
@@ -20,8 +20,8 @@ ActiveStorage::Blob.create!([
   {key: "gb1wrhutupctpts8v28ys4t71ior", filename: "me.jpg", content_type: "image/jpeg", metadata: {"identified"=>true, "analyzed"=>true}, byte_size: 9658635, checksum: "8eYmoGWTcxMR2yB0pqIXGg=="}
 ])
 ActiveStorage::Attachment.create!([
-  {name: "avatar", record_type: "User", record_id: 14, blob_id: 1},
-  {name: "avatar", record_type: "User", record_id: 27, blob_id: 6},
-  {name: "avatar", record_type: "User", record_id: 28, blob_id: 7},
-  {name: "avatar", record_type: "User", record_id: 31, blob_id: 10}
+  {name: "avatar", record_type: "User", record_id: 1, blob_id: 1},
+  {name: "avatar", record_type: "User", record_id: 1, blob_id: 6},
+  {name: "avatar", record_type: "User", record_id: 2, blob_id: 7},
+  {name: "avatar", record_type: "User", record_id: 3, blob_id: 10}
 ])
