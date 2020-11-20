1978a22f226a6a9ff5eb7a65901d00c614e6ee8e
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-20 06:32:47 +0400

    create method for navigation views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def show_userName(current_user)
+    if signed_in?
+      "<li class='nav-item'>
+      <p class='nav-link text-white'>#{current_user.name}</p>
+    </li>
+    <li class='nav-item'>
+       #{link_to 'logout', destroy_user_session_path, class: 'nav-link', method: :delete}
+    </li>".html_safe
+    else
+      "<li class='nav-item'>
+      #{link_to 'Log in / Sign up', new_user_registration_path, class: 'nav-link'}
+    </li>".html_safe
+    end
+  end
 end
