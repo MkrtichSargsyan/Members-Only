f5dcf0e6e69e070fdc5c5496e4ffd2f1a261cbb4
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-20 09:00:03 +0400

    fix helper name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(
+#   name: 'Tyler Durden',
+#   email: 'tylerr@gmail.com',
+#   password: 'aaaaaa',
+#   avatar: File.new("logo.jpeg")
+# )
